8c3e7b9409d1d18f3fcd327f979343cd1279149c
Arturo <frisix@gmail.com>
2023-07-25 12:07:10 +0200

    Actualiados los parametros de inicio de vscode. Creo que es mejor eliminarlos

--- ArtImageTaggerConsole/Program.cs
+++ ArtImageTaggerConsole/Program.cs
@@ -5,14 +5,36 @@
 
 namespace ArtImageTaggerConsole  {
 
-    using GoogleTakeoutJSONParser;
+    using System;
+    using System.IO;
+    using System.Linq;
 
     class Program {
     static void Main(string[] args) {
+
+        string directoryPath = ".";
         
-        GoogleTakeoutJSONParser parser = new GoogleTakeoutJSONParser();
+        if (args.Length == 0) {
+            Console.WriteLine("Usando ruta por defecto: " + directoryPath);
+        } else if (args.Length == 1) {
+            directoryPath = args[0];
+            Console.WriteLine("Usando ruta: " + directoryPath);
+        } else {
+            Console.WriteLine("Uso: ArtImageTaggerConsole.exe [ruta]");
+            return;
+        }
+
         
-        Console.WriteLine("Hello, World!");
+        var allowedExtensions = new [] {".jpg",".jpeg"}; 
+
+
+        var files = Directory.GetFiles(directoryPath, "*.*", SearchOption.AllDirectories)
+                             .Where(file => allowedExtensions.Any(file.ToLower().EndsWith));
+
+        foreach (var file in files)
+        {
+            Console.WriteLine(file);
+        }
 
 
     }
